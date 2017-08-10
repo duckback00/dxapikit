a08f4ae5f9f6ba71b6cd1c89676df38694fccf03
Alan Bitterman <duckback00@gmail.com>
2017-08-10 00:22:29 -0400

    API calls to change the state of VDB's

--- API/vdb_init.sh
+++ API/vdb_init.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
-#v1.1
+#!/bin/sh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2017 by Delphix. All rights reserved.
+#
+# Program Name : vdb_init.sh
+# Description  : Delphix API calls to change the state of VDB's
+# Author       : Alan Bitterman
+# Created      : 2017-08-09
+# Version      : v1.0.0
+#
+# Requirements :
+#  1.) curl and jq command line libraries
+#  2.) Populate Delphix Engine Connection Information . ./delphix_engine.conf
+#  3.) Include ./jqJSON_subroutines.sh
+#
+# Initeractive Usage: ./vdb_init.sh
+#
+# Non-Interactive Usage: ./vdb_init.sh [start | stop | enable | disable | status | delete] [VDB_Name] 
 #
 # Delphix Doc's Reference: 
-#    https://docs.delphix.com/pages/viewpage.action?pageId=51970750
+#    https://docs.delphix.com/docs/reference/web-service-api-guide/api-cookbook-common-tasks-workflows-and-examples/api-cookbook-stop-start-a-vdb
 #
 #########################################################
 ## Subroutines ...
