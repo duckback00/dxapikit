45f9276a667e6bd85b42d621a6f6d85704ec09d6
Alan Bitterman <duckback00@gmail.com>
2017-08-09 23:22:06 -0400

    About API - General Delphix Engine Information
    About API - General Delphix Engine Information with some useful JSON parsing and array techniques

--- API/about.sh
+++ API/about.sh
@@ -1,4 +1,31 @@
-
+#!/bin/sh
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2017 by Delphix. All rights reserved.
+#
+# Program Name : about.sh
+# Description  : Delphix API about call 
+# Author       : Alan Bitterman
+# Created      : 2017-08-12
+# Version      : v1.0.0
+#
+# Requirements :
+#  1.) curl and jq command line libraries 
+#  2.) Populate Delphix Engine Connection Information . ./delphix_engine.conf
+#
+# Usage: ./about.sh
+#
 #########################################################
 #                   DELPHIX CORP                        #
 #########################################################
@@ -129,7 +156,6 @@ do
    echo "$key |${tmp}|"; 
 done
 IFS=
-
 
 
 # 
