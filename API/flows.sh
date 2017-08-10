4d2b7da0aa6933def3b5c1f138716437070daef0
Alan Bitterman <duckback00@gmail.com>
2017-08-09 23:58:34 -0400

    dSource, VDB, and Snapshot Timeflows Examples

--- API/flows.sh
+++ API/flows.sh
@@ -1,6 +1,32 @@
-#!/bin/bash
-#v1.x
-
+#!/bin/sh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2017 by Delphix. All rights reserved.
+#
+# Program Name : flows.sh
+# Description  : Delphix API timeflows examples
+# Author       : Alan Bitterman
+# Created      : 2017-08-09
+# Version      : v1.0.0
+#
+# Requirements :
+#  1.) curl and jq command line libraries
+#  2.) Include ./jqJSON_subroutines.sh
+#  3.) Populate Delphix Engine Connection Information . ./delphix_engine.conf
+#
+# Interactive Usage: ./flows.sh
+#
 #########################################################
 ## Subroutines ...
 
