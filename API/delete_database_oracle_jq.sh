5e6e0e321a591c85691e1fb63a533365cd5b2cbb
Alan Bitterman <duckback00@gmail.com>
2017-08-11 10:47:31 -0400

    Delphix API to Delete an Oracle VDB Example

--- API/delete_database_oracle_jq.sh
+++ API/delete_database_oracle_jq.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
-#v1.x
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2017 by Delphix. All rights reserved.
+#
+# Program Name : delete_database_oracle_jq.sh
+# Description  : Delphix API to Delete an Oracle VDB Example
+# Author       : Alan Bitterman
+# Created      : 2017-08-09
+# Version      : v1.0.0
+#
+# Requirements :
+#  1.) curl and jq command line libraries
+#  2.) Populate Delphix Engine Connection Information . ./delphix_engine.conf
+#  3.) Include ./jqJSON_subroutines.sh
+#
+# Interactive Usage: ./delete_database_oracle_jq.sh
+#
+# Non-Interactive Usage: ./delete_database_oracle_jq.sh [VDB_name]
+#
+# NOTES:
+#  see vdb_init.sh for delete and other init operations 
+#
+#########################################################
+#                   DELPHIX CORP                        #
+#         NO CHANGES REQUIRED BELOW THIS POINT          #
+#########################################################
 
 #########################################################
 ## Subroutines ...
@@ -7,40 +43,14 @@
 source ./jqJSON_subroutines.sh
 
 #########################################################
-#                   DELPHIX CORP                        #
-#########################################################
-#Parameter Initialization
+## Parameter Initialization ...
 
 . ./delphix_engine.conf
 
-#
-# Required for Database Delete ...
-#
-
-#
-# Command Line Arguments ...
-#
-SOURCE_SID=$1
-if [[ "${SOURCE_SID}" == "" ]]
-then
-   SOURCE_SID="VBITT2"
-fi;
-export SOURCE_SID
-
-SOURCE_SID=$(sed -e 's/ /%20/g' -e 's/!/%21/g' -e 's/"/%22/g' -e 's/#/%23/g' -e 's/\&/%26/g' -e 's/'\''/%28/g' -e 's/(/%28/g' -e 's/)/%29/g' -e 's/:/%3A/g' -e 's/\//%2F/g'<<<$SOURCE_SID);
-
-###HARD CODE###SOURCE_SID="VBITT2"            # dSource name used to get db container reference value 
-
-echo "Deleting dSource of Virtual database ${SOURCE_SID} ... "
-
-#########################################################
-#         NO CHANGES REQUIRED BELOW THIS POINT          #
-#########################################################
-
-echo "Authenticating on ${BaseURL}"
-
 #########################################################
 ## Session and Login ...
+
+echo "Authenticating on ${BaseURL}"
 
 RESULTS=$( RestSession "${DMUSER}" "${DMPASS}" "${BaseURL}" "${COOKIE}" "${CONTENT_TYPE}" )
 #echo "Results: ${RESULTS}"
@@ -53,10 +63,42 @@ fi
 echo "Session and Login Successful ..."
 
 #########################################################
-## Get database container
+## Get database info ...
 
 STATUS=`curl -s -X GET -k ${BaseURL}/database -b "${COOKIE}" -H "${CONTENT_TYPE}"`
 RESULTS=$( jqParse "${STATUS}" "status" )
+##echo "${STATUS}" | jq '.'
+
+#########################################################
+#
+# Command Line Arguments ...
+#
+
+SOURCE_SID=$1
+if [[ "${SOURCE_SID}" == "" ]]
+then
+   VDB_NAMES=`echo "${STATUS}" | jq --raw-output '.result[] | select(.type=="OracleDatabaseContainer") | .name '`
+   echo "---------------------------------"
+   echo "VDB Names: [copy-n-paste]"
+   echo "${VDB_NAMES}"
+   echo " "
+
+   echo "Please Enter dSource or VDB Name (case sensitive): "
+   read SOURCE_SID
+   if [ "${SOURCE_SID}" == "" ]
+   then
+      echo "No dSource or VDB Name Provided, Exiting ..."
+      exit 1;
+   fi
+fi;
+export SOURCE_SID
+
+SOURCE_SID=$(sed -e 's/ /%20/g' -e 's/!/%21/g' -e 's/"/%22/g' -e 's/#/%23/g' -e 's/\&/%26/g' -e 's/'\''/%28/g' -e 's/(/%28/g' -e 's/)/%29/g' -e 's/:/%3A/g' -e 's/\//%2F/g'<<<$SOURCE_SID);
+
+echo "Deleting dSource of Virtual database ${SOURCE_SID} ... "
+
+#########################################################
+## Get database container reference ...
 
 #
 # Parse out container reference for name of $SOURCE_SID ...
