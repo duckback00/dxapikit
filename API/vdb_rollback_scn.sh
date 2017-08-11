c031e5a8b2b267ebd81698e58b954bc374eaaf71
Alan Bitterman <duckback00@gmail.com>
2017-08-11 15:24:44 -0400

    Delphix API Rollback VDB by Timeflow SCN

--- API/vdb_rollback_scn.sh
+++ API/vdb_rollback_scn.sh
@@ -1,25 +1,51 @@
 #!/bin/bash
-#v1.1
 #
-# sample script to perform basic operations on a  VDB.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2017 by Delphix. All rights reserved.
+#
+# Program Name : vdb_rollback_scn.sh
+# Description  : Delphix API Rollback VDB by Timeflow SCN
+# Author       : Alan Bitterman
+# Created      : 2017-08-09
+# Version      : v1.0.0
+#
+# Requirements :
+#  1.) curl and jq command line libraries
+#  2.) Populate Delphix Engine Connection Information . ./delphix_engine.conf
+#  3.) Include ./jqJSON_subroutines.sh
+#  4.) Change values below as required
+#
+# Interactive Usage: ./vdb_rollback_scn.sh
 #
 # Delphix Docs Reference:
-#   https://docs.delphix.com/display/DOCS/API+Cookbook%3A+Refresh+VDB
+#   https://docs.delphix.com/docs/reference/web-service-api-guide/api-cookbook-common-tasks-workflows-and-examples
 #
-#
+#########################################################
+#                   DELPHIX CORP                        #
+#         NO CHANGES REQUIRED BELOW THIS POINT          #
+#########################################################
+
 #########################################################
 ## Subroutines ...
 
 source ./jqJSON_subroutines.sh
 
 #########################################################
-#                   DELPHIX CORP                        #
-#########################################################
+## Parameter Initialization ...
 
 . ./delphix_engine.conf
 
-#########################################################
-#         NO CHANGES REQUIRED BELOW THIS POINT          #
 #########################################################
 
 #
@@ -39,8 +65,9 @@ fi;
 export SOURCE_SID
 
 #########################################################
-# Authentication ...
-#
+## Authentication ...
+
+echo "Authenticating on ${BaseURL}"
 
 RESULTS=$( RestSession "${DMUSER}" "${DMPASS}" "${BaseURL}" "${COOKIE}" "${CONTENT_TYPE}" )
 #echo "Results: ${RESULTS}"
@@ -64,17 +91,6 @@ RESULTS=$( jqParse "${STATUS}" "status" )
 #
 CONTAINER_REFERENCE=`echo ${STATUS} | jq --raw-output '.result[] | select(.name=="'"${SOURCE_SID}"'") | .reference '`
 echo "database container reference: ${CONTAINER_REFERENCE}"
-
-#########################################################
-## Get provision source database container
-
-#STATUS=`curl -s -X GET -k ${BaseURL}/database/${CONTAINER_REFERENCE} -b "${COOKIE}" -H "${CONTENT_TYPE}"`
-#RESULTS=$( jqParse "${STATUS}" "status" )
-##echo "results> $RESULTS"
-
-##echo "${STATUS}"
-#PARENT_SOURCE=`echo ${STATUS} | jq --raw-output '.result | select(.reference=="'"${CONTAINER_REFERENCE}"'") | .provisionContainer '`
-#echo "provision source container: ${PARENT_SOURCE}"
 
 #########################################################
 ## List timeflows for the container reference
@@ -123,7 +139,9 @@ then
    exit 1;
 fi
 
-
+#
+# Build JSON String ...
+#
 json="{
     \"type\": \"OracleRollbackParameters\",
     \"timeflowPointParameters\": {
@@ -135,6 +153,7 @@ json="{
 }"
 
 echo "json> ${json}"
+echo "Please wait, Submitting Rollback Job Request ..."
 
 #
 # Submit VDB operations request ...
