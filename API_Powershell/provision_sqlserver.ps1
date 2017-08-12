e37eb8753f3abc34201ace8330b69e195501f2d0
Alan Bitterman <duckback00@gmail.com>
2017-08-12 01:38:43 -0400

    Delphix PowerShell API provision VDB Example

--- API_Powershell/provision_sqlserver.ps1
+++ API_Powershell/provision_sqlserver.ps1
@@ -1,128 +1,82 @@
-# Filename: provision_sqlserver.ps1
-# Description: Delphix Powershell Sample Authentication Script ...
-# Date: 2016-08-02
-# Author: Bitt...
 #
-
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2017 by Delphix. All rights reserved.
+#
+# Program Name : provision_sqlserver.ps1
+# Description  : Delphix PowerShell API provision VDB Example  
+# Author       : Alan Bitterman
+# Created      : 2017-08-09
+# Version      : v1.0.0
+#
+# Requirements :
+#  1.) curl command line libraries
+#  2.) Populate Delphix Engine Connection Information . .\delphix_engine_conf.ps1
+#  3.) Change values below as required
+#
+# Usage: ./provision_sqlserver.ps1 
+#
 #########################################################
 #                   DELPHIX CORP                        #
+# Please make changes to the parameters below as req'd! #
 #########################################################
-
-#########################################################
-# Parameter Initialization
-
-#
-# Variables ...
-#
-$nl = [Environment]::NewLine
-$BaseURL = "http://172.16.160.195/resources/json/delphix"
-$cookie = "cookies.txt"
-$delphix_user = "delphix_admin"
-$delphix_pass = "delphix"
-
-$CONTENT_TYPE="Content-Type: application/json"
-$DELAYTIMESEC=10
 
 #
 # Required for Provisioning Virtual Database ...
 #
-$SOURCE_SID="delphixdb"           # dSource name used to get db container reference value
+$SOURCE_SID="delphix_demo"        # dSource name used to get db container reference value
 
-$VDB_NAME="VBITT"                 # Delphix VDB Name
-$TARGET_GRP="Windows_Target"      # Delphix Engine Group Name
-$TARGET_ENV="Window Target"       # Target Environment used to get repository reference value 
+$VDB_NAME="Vdelphix_demo"         # Delphix VDB Name
+$TARGET_GRP="Windows Source"      # Delphix Engine Group Name
+$TARGET_ENV="Windows Target"      # Target Environment used to get repository reference value 
 $TARGET_REP="MSSQLSERVER"         # Target Environment Repository / Instance name
 
 #########################################################
 #         NO CHANGES REQUIRED BELOW THIS POINT          #
 #########################################################
 
-#
-# Local Functions ...
-#
-. .\parseFunctions.ps1
+#########################################################
+## Local Functions ...
+
+. .\delphixFunctions.ps1
 
 #########################################################
-#
-# Authentication ...
-#
-write-output "Authenticating on ${BaseURL}"
+## Parameter Initialization ...
 
-# 
-# Session JSON Data ...
-#
-#write-output "Creating session.json file ..."
-$json = @"
-{
-    "type": "APISession",
-    "version": {
-        "type": "APIVersion",
-        "major": 1,
-        "minor": 7,
-        "micro": 0
-    }
-}
-"@
+. .\delphix_engine_conf.ps1
 
-#
-# Output File using UTF8 encoding ...
-#
-write-output $json | Out-File "session.json" -encoding utf8
+#########################################################
+## Authentication ...
 
-#
-# Delphix Curl Session API ...
-#
-#write-output "Calling Session API ... "
-$results = (curl --insecure -c "${cookie}" -sX POST -H "${CONTENT_TYPE}" -d "@session.json" -k ${BaseURL}/session)
-#write-output "Session API Results: ${results}"
+write-output "Authenticating on ${BaseURL} ... ${nl}"
 
-# Convert Results String to JSON Object and Get Results Status ...
+$results=RestSession "${DMUSER}" "${DMPASS}" "${BaseURL}" "${COOKIE}" "${CONTENT_TYPE}" 
+#write-output "${nl} Results are ${results} ..."
+
 $o = ConvertFrom-Json20 $results
-$status=$o.status			#echo "Status ... $status ${nl}"
+$status=$o.status                       #echo "Status ... $status ${nl}"
 if ("${status}" -ne "OK") {
    echo "Job Failed with ${status} Status ${nl} $results ${nl}"
    exit 1
 }
 
-#
-# Login JSON Data ...
-# 
-#write-output "Creating login.json file ..."
-$json = @"
-{
-    "type": "LoginRequest",
-    "username": "${delphix_user}",
-    "password": "${delphix_pass}"
-}
-"@
-
-#
-# Output File using UTF8 encoding ...
-#
-write-output $json | Out-File "login.json" -encoding utf8
-
-#
-# Delphix Curl Login API ...
-#
-#write-output "Calling Login API ..."
-$results = (curl --insecure -b "${cookie}" -sX POST -H "${CONTENT_TYPE}" -d "@login.json" -k ${BaseURL}/login)
-#write-output "Login API Results: ${results}"
-
-# Convert Results String to JSON Object and Get Results Status ...
-$o = ConvertFrom-Json20 $results
-$status=$o.status			#echo "Status ... $status ${nl}"
-if ("${status}" -ne "OK") {
-   echo "Job Failed with ${status} Status ${nl} $results ${nl}"
-   exit 1
-}
 echo "Login Successful ..."
 
-
 #########################################################
-## Get or Create Group
+## Get Group Reference ...
 
 #write-output "${nl}Calling Group API ...${nl}"
-$results = (curl --insecure -b "${cookie}" -sX GET -H "${CONTEXT_TYPE}" -k ${BaseURL}/group)
+$results = (curl.exe --insecure -sX GET -k ${BaseURL}/group -b "${COOKIE}" -H "${CONTENT_TYPE}")
 #write-output "Group API Results: ${results}"
 
 # Convert Results String to JSON Object and Get Results Status ...
@@ -140,12 +94,11 @@ $b = $a | where { $_.name -eq "${TARGET_GRP}" } | Select-Object
 $GROUP_REFERENCE=$b.reference
 echo "group reference: ${GROUP_REFERENCE}"
 
-
 #########################################################
-## Get database container
+## Get database container ...
 
 #write-output "${nl}Calling Database API ...${nl}"
-$results = (curl --insecure -b "${cookie}" -sX GET -H "${CONTEXT_TYPE}" -k ${BaseURL}/database)
+$results = (curl.exe --insecure -sX GET -k ${BaseURL}/database -b "${COOKIE}" -H "${CONTENT_TYPE}")
 #write-output "Database API Results: ${results}"
 
 # Convert Results String to JSON Object and Get Results Status ...
@@ -166,10 +119,10 @@ echo "container reference: ${CONTAINER_REFERENCE}"
 #CONTAINER_REFERENCE=$( parseResults "${STATUS}" "${SOURCE_SID}" "name" "reference" )
 
 #########################################################
-## Get Environment reference  
+## Get Environment reference ...
 
 #write-output "${nl}Calling environment API ...${nl}"
-$results = (curl --insecure -b "${cookie}" -sX GET -H "${CONTEXT_TYPE}" -k ${BaseURL}/environment)
+$results = (curl.exe --insecure -sX GET -k ${BaseURL}/environment -b "${COOKIE}" -H "${CONTENT_TYPE}")
 #write-output "environment API Results: ${results}"
 
 $o = ConvertFrom-Json20 $results
@@ -182,10 +135,10 @@ echo "env reference: ${ENV_REFERENCE}"
 #ENV_REFERENCE=$( parseResults "${STATUS}" "${TARGET_ENV}" "name" "reference" )
 
 #########################################################
-## Get Repository reference  
+## Get Repository reference ...
 
 #write-output "${nl}Calling repository API ...${nl}"
-$results = (curl --insecure -b "${cookie}" -sX GET -H "${CONTEXT_TYPE}" -k ${BaseURL}/repository)
+$results = (curl.exe --insecure -sX GET -k ${BaseURL}/repository -b "${COOKIE}" -H "${CONTENT_TYPE}")
 #write-output "repository API Results: ${results}"
 
 # Convert Results String to JSON Object and Get Results Status ...
@@ -203,62 +156,57 @@ $b = $a | where { $_.name -eq "${TARGET_REP}" } | Select-Object
 $REP_REFERENCE=$b.reference
 echo "repository reference: ${REP_REFERENCE}"
 
-
 #########################################################
 ## Provision a SQL Server Database ...
 
 $json = @"
 {
-    "type": "MSSqlProvisionParameters",
-    "container": {
-        "type": "MSSqlDatabaseContainer",
-        "name": "${VDB_NAME}",
-        "group": "${GROUP_REFERENCE}",
-        "sourcingPolicy": {
-            "type": "SourcingPolicy",
-            "loadFromBackup": false,
-            "logsyncEnabled": false
+    \"type\": \"MSSqlProvisionParameters\",
+    \"container\": {
+        \"type\": \"MSSqlDatabaseContainer\",
+        \"name\": \"${VDB_NAME}\",
+        \"group\": \"${GROUP_REFERENCE}\",
+        \"sourcingPolicy\": {
+            \"type\": \"SourcingPolicy\",
+            \"loadFromBackup\": false,
+            \"logsyncEnabled\": false
         },
-        "validatedSyncMode": "TRANSACTION_LOG"
+        \"validatedSyncMode\": \"TRANSACTION_LOG\"
     },
-    "source": {
-        "type": "MSSqlVirtualSource",
-        "operations": {
-            "type": "VirtualSourceOperations",
-            "configureClone": [],
-            "postRefresh": [],
-            "postRollback": [],
-            "postSnapshot": [],
-            "preRefresh": [],
-            "preSnapshot": []
+    \"source\": {
+        \"type\": \"MSSqlVirtualSource\",
+        \"operations\": {
+            \"type\": \"VirtualSourceOperations\",
+            \"configureClone\": [],
+            \"postRefresh\": [],
+            \"postRollback\": [],
+            \"postSnapshot\": [],
+            \"preRefresh\": [],
+            \"preSnapshot\": []
         }
     },
-    "sourceConfig": {
-        "type": "MSSqlSIConfig",
-        "linkingEnabled": false,
-        "repository": "${REP_REFERENCE}",
-        "databaseName": "${VDB_NAME}",
-        "recoveryModel": "SIMPLE",
-        "instance": {
-            "type": "MSSqlInstanceConfig",
-            "host": "${ENV_REFERENCE}"
+    \"sourceConfig\": {
+        \"type\": \"MSSqlSIConfig\",
+        \"linkingEnabled\": false,
+        \"repository\": \"${REP_REFERENCE}\",
+        \"databaseName\": \"${VDB_NAME}\",
+        \"recoveryModel\": \"SIMPLE\",
+        \"instance\": {
+            \"type\": \"MSSqlInstanceConfig\",
+            \"host\": \"${ENV_REFERENCE}\"
         }
     },
-    "timeflowPointParameters": {
-        "type": "TimeflowPointSemantic",
-        "container": "${CONTAINER_REFERENCE}",
-        "location": "LATEST_SNAPSHOT"
+    \"timeflowPointParameters\": {
+        \"type\": \"TimeflowPointSemantic\",
+        \"container\": \"${CONTAINER_REFERENCE}\",
+        \"location\": \"LATEST_SNAPSHOT\"
     }
 }
 "@
 
-#
-# Output File using UTF8 encoding ...
-#
-write-output $json | Out-File "provision_sqlserver.json" -encoding utf8
 
 #write-output "${nl}Calling database provision API ...${nl}"
-$results = (curl --insecure -b "${cookie}" -sX POST -H "Content-Type: application/json" -d "@provision_sqlserver.json" -k ${BaseURL}/database/provision)
+$results = (curl.exe --insecure -sX POST -k ${BaseURL}/database/provision -b "${COOKIE}" -H "${CONTENT_TYPE}" -d "${json}")
 write-output "database provision API Results: ${results}"
 
 # Convert Results String to JSON Object and Get Results Status ...
@@ -287,11 +235,11 @@ sleep 2
 # Job Information ...
 #
 #write-output "${nl}Calling job API ...${nl}"
-$results = (curl --insecure -b "${cookie}" -sX GET -H "${CONTEXT_TYPE}" -k ${BaseURL}/job/${JOB})
+$results = (curl.exe --insecure -sX GET -k ${BaseURL}/job/${JOB} -b "${COOKIE}" -H "${CONTENT_TYPE}")
 #write-output "job API Results: ${results}"
 
 $o = ConvertFrom-Json20 "${results}"		#$o
-$a = $o.result								#$a
+$a = $o.result					#$a
 
 #
 # Get Job Status and Job Information ...
@@ -310,7 +258,7 @@ DO
   $d = Get-Date
   echo "Current status as of ${d} : ${JOBSTATE} : ${PERCENTCOMPLETE}% Completed"
   sleep ${DELAYTIMESEC}
-  $results = (curl --insecure -b "${cookie}" -sX GET -H "${CONTEXT_TYPE}" -k ${BaseURL}/job/${JOB})
+  $results = (curl.exe --insecure -sX GET -k ${BaseURL}/job/${JOB} -b "${COOKIE}" -H "${CONTENT_TYPE}")
   $o = ConvertFrom-Json20 "${results}"
   $a = $o.result
   $JOBSTATE=$a.jobState
@@ -326,7 +274,6 @@ if ("${JOBSTATE}" -eq "COMPLETED") {
 } else {
    echo "Job Failed with ${JOBSTATE} Status ${nl}"
 }
-
 
 ############## E O F ####################################
 echo " "
