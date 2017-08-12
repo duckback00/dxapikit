90a0fc60023ddc546a4b92230ef80be6153cc584
Alan Bitterman <duckback00@gmail.com>
2017-08-12 01:34:19 -0400

    Delphix Engine Configuration Parameters

---
+++ API_Powershell/delphix_engine_conf.ps1
@@ -0,0 +1,10 @@
+#
+# Variables ...
+#
+$nl = [Environment]::NewLine
+$BaseURL = "http://172.16.160.195/resources/json/delphix"
+$CONTENT_TYPE = "Content-Type: application/json"
+$COOKIE = "cookies.txt"
+$DMUSER = "delphix_admin"
+$DMPASS = "delphix"
+$DELAYTIMESEC=10
