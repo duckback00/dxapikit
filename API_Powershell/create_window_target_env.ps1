253d5625eec8159630b57c83432758a3e2920412
Alan Bitterman <duckback00@gmail.com>
2017-08-12 01:45:52 -0400

    Create a Delphix SQL Server Environment

--- API_Powershell/create_window_target_env.ps1
+++ API_Powershell/create_window_target_env.ps1
@@ -10,61 +10,50 @@
 $nl = [Environment]::NewLine
 $BaseURL = "http://172.16.160.195/resources/json/delphix"
 $cookie = "cookies.txt"
+$user = "delphix_admin"
+$pass = "delphix"
 
 # 
 # Session JSON Data ...
 #
-write-output "${nl}Creating session.json file ..."
 $json = @"
 {
-    "type": "APISession",
-    "version": {
-        "type": "APIVersion",
-        "major": 1,
-        "minor": 7,
-        "micro": 0
+    \"type\": \"APISession\",
+    \"version\": {
+        \"type\": \"APIVersion\",
+        \"major\": 1,
+        \"minor\": 7,
+        \"micro\": 0
     }
 }
 "@
 
-#
-# Output File using UTF8 encoding ...
-#
-write-output $json | Out-File "session.json" -encoding utf8
 
 #
 # Delphix Curl Session API ...
 #
 write-output "${nl}Calling Session API ...${nl}"
-$results = (curl --insecure -c "${cookie}" -sX POST -H "Content-Type: application/json" -d "@session.json" -k ${BaseURL}/session)
+$results = (curl --insecure -c "${cookie}" -sX POST -H "Content-Type: application/json" -d "${json}" -k ${BaseURL}/session)
 write-output "Session API Results: ${results}"
 
 #
 # Login JSON Data ...
 # 
-write-output "${nl}Creating login.json file ..."
-$user = "delphix_admin"
-$pass = "delphix"
 $json = @"
 {
-    "type": "LoginRequest",
-    "username": "${user}",
-    "password": "${pass}"
+    \"type\": \"LoginRequest\",
+    \"username\": \"${user}\",
+    \"password\": \"${pass}\"
 }
 "@
 
-#
-# Output File using UTF8 encoding ...
-#
-write-output $json | Out-File "login.json" -encoding utf8
 
 #
 # Delphix Curl Login API ...
 #
 write-output "${nl}Calling Login API ...${nl}"
-$results = (curl --insecure -b "${cookie}" -sX POST -H "Content-Type: application/json" -d "@login.json" -k ${BaseURL}/login)
+$results = (curl --insecure -b "${cookie}" -sX POST -H "Content-Type: application/json" -d "${json}" -k ${BaseURL}/login)
 write-output "Login API Results: ${results}"
-
 
 #######################################################################
 #
@@ -72,41 +61,34 @@ write-output "Login API Results: ${results}"
 #
 $json = @"
 {
-    "type": "HostEnvironmentCreateParameters",
-    "primaryUser": {
-        "type": "EnvironmentUser",
-        "name": "DELPHIX\\delphix_admin",
-        "credential": {
-            "type": "PasswordCredential",
-            "password": "delphix"
+    \"type\": \"HostEnvironmentCreateParameters\",
+    \"primaryUser\": {
+        \"type\": \"EnvironmentUser\",
+        \"name\": \"DELPHIX\\delphix_admin\",
+        \"credential\": {
+            \"type\": \"PasswordCredential\",
+            \"password\": \"delphix\"
         }
     },
-    "hostEnvironment": {
-        "type": "WindowsHostEnvironment",
-        "name": "Window Target"
+    \"hostEnvironment\": {
+        \"type\": \"WindowsHostEnvironment\",
+        \"name\": \"Windows Target\"
     },
-    "hostParameters": {
-        "type": "WindowsHostCreateParameters",
-        "host": {
-            "type": "WindowsHost",
-            "address": "172.16.160.183",
-            "connectorPort": 9100
+    \"hostParameters\": {
+        \"type\": \"WindowsHostCreateParameters\",
+        \"host\": {
+            \"type\": \"WindowsHost\",
+            \"address\": \"172.16.160.196\",
+            \"connectorPort\": 9100
         }
     }
 }
 "@
 
-write-output $json | Out-File "create_env.json" -encoding utf8
 
 write-output "${nl}Calling Create Environment API ...${nl}"
-$results = (curl --insecure -b "${cookie}" -sX POST -H "Content-Type: application/json" -d "@create_env.json" -k ${BaseURL}/environment)
+$results = (curl --insecure -b "${cookie}" -sX POST -H "Content-Type: application/json" -d "${json}" -k ${BaseURL}/environment)
 write-output "Environment API Results: ${results}"
-
-#curl -X POST -k --data @- http://172.16.160.195/resources/json/delphix/environment \
-#    -b ~/cookies.txt -H "Content-Type: application/json" <<EOF
-
-
-
 
 #
 # The End is Near ...
