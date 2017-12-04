fe0198fd0abd9ba7b0755be5e629912fc798b89b
Alan Bitterman <duckback00@gmail.com>
2017-12-04 02:05:26 -0500

    Create a new Bookmark from Latest

---
+++ API/jetstream_bookmark_create_from_latest_jq.sh
@@ -0,0 +1,366 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2017 by Delphix. All rights reserved.
+#
+# Program Name : jetstream_bookmark_create_from_latest_jq.sh 
+# Description  : Delphix API to create a JetStream Bookmark in Active Branch
+# Author       : Alan Bitterman
+# Created      : 2017-11-20
+# Version      : v1.2
+#
+# Requirements :
+#  1.) curl and jq command line libraries
+#  2.) Populate Delphix Engine Connection Information . ./delphix_engine.conf
+#  3.) Include ./jqJSON_subroutines.sh
+#  4.) Change values below as required
+#
+# Interactive Usage: 
+# ./jetstream_bookmark_create_jq.sh
+#
+# Non-interactive Usage:
+# ./jetstream_bookmark_create_from_latest_jq.sh [template_name] [container_name] [bookmark_name]    SHARED         TAGS
+# ./jetstream_bookmark_create_from_latest_jq.sh [template_name] [container_name] [bookmark_name] [true|false] ["tag1","tag2"] 
+#
+# Tags are arrays and must be "quoted" if more than one and delimited by a comma
+# 
+# ./jetstream_bookmark_create_from_latest_jq.sh tpl dc BM3 false '"Hey","There"'
+#
+# Non-interactive using hardcode defaults iff set: 
+# ./jetstream_bookmark_create_from_latest_jq.sh 
+#
+#########################################################
+#                   DELPHIX CORP                        #
+# Please make changes to the parameters below as req'd! #
+#########################################################
+
+#########################################################
+## Parameter Initialization ...
+
+. ./delphix_engine.conf
+
+#
+# Default Values if not provided on Command Line ... 
+#
+# For non-interactive defaults ...
+# 
+#DEF_JS_TEMPLATE="tpl"
+#DEF_JS_CONTAINER_NAME="dc"
+#DEF_JS_BOOK_NAME="Wally_${DT}"   # JetStream Bookmark Name_append timestamp
+#DEF_SHARED="false"               # Share Bookmark true/false
+#DEF_TAGS='"API","Created"'       # Tags Array Values
+#
+# For full interactive option, set default values to nothing ...
+#
+DEF_JS_TEMPLATE=""
+DEF_JS_CONTAINER_NAME=""
+DEF_JS_BOOK_NAME=""
+DEF_SHARED=""
+DEF_TAGS=""
+
+#########################################################
+#         NO CHANGES REQUIRED BELOW THIS POINT          #
+#########################################################
+
+#########################################################
+## Subroutines ...
+
+source ./jqJSON_subroutines.sh
+
+#########################################################
+## Session and Login ...
+
+echo "Authenticating on ${BaseURL}"
+
+RESULTS=$( RestSession "${DMUSER}" "${DMPASS}" "${BaseURL}" "${COOKIE}" "${CONTENT_TYPE}" )
+#echo "Results: ${RESULTS}"
+if [ "${RESULTS}" != "OK" ]
+then
+   echo "Error: Exiting ..."
+   exit 1;
+fi
+
+#########################################################
+## Get Template Reference ...
+
+#echo "Getting Jetstream Template Reference ..."
+STATUS=`curl -s -X GET -k ${BaseURL}/jetstream/template -b "${COOKIE}" -H "${CONTENT_TYPE}"`
+RESULTS=$( jqParse "${STATUS}" "status" )
+#echo "${STATUS}" | jq "."
+
+JS_TEMPLATE="${1}"
+if [[ "${JS_TEMPLATE}" == "" ]]
+then
+   ZTMP="Template Name"
+   if [[ "${DEF_JS_TEMPLATE}" == "" ]]
+   then
+      TMP=`echo "${STATUS}" | jq --raw-output '.result[] | .name '`
+      echo "---------------------------------"
+      echo "${ZTMP}s: [copy-n-paste]"
+      echo "${TMP}"
+      echo " "
+      echo "Please Enter ${ZTMP} (case sensitive): "
+      read JS_TEMPLATE
+      if [ "${JS_TEMPLATE}" == "" ]
+      then
+         echo "No ${ZTMP} Provided, Exiting ..."
+         exit 1;
+      fi
+   else
+      echo "No ${ZTMP} Provided, using Default ..."
+      JS_TEMPLATE=${DEF_JS_TEMPLATE}
+   fi
+fi
+echo "template name: ${JS_TEMPLATE}"
+
+#
+# Parse ...
+#
+JS_TPL_REF=`echo "${STATUS}" | jq --raw-output '.result[] | select(.name=="'"${JS_TEMPLATE}"'") | .reference '`
+echo "template reference: ${JS_TPL_REF}"
+
+if [[ "${JS_TPL_REF}" == "" ]]
+then
+   echo "${ZTMP} Reference ${JS_TPL_REF} for ${JS_TEMPLATE} not found, Exiting ..."
+   exit 1
+fi
+
+#########################################################
+## Get container reference...
+
+#echo "Getting Jetstream Template Container Reference ..."
+STATUS=`curl -s -X GET -k ${BaseURL}/jetstream/container -b "${COOKIE}" -H "${CONTENT_TYPE}"`
+RESULTS=$( jqParse "${STATUS}" "status" )
+#echo "${STATUS}" | jq "."
+
+JS_CONTAINER_NAME="${2}"
+if [[ "${JS_CONTAINER_NAME}" == "" ]]
+then
+   ZTMP="Container Name"
+   if [[ "${DEF_JS_CONTAINER_NAME}" == "" ]]
+   then
+      TMP=`echo "${STATUS}" | jq --raw-output '.result[] | select (.template=="'"${JS_TPL_REF}"'") | .name '`
+      echo "---------------------------------"
+      echo "${ZTMP}s: [copy-n-paste]"
+      echo "${TMP}"
+      echo " "
+      echo "Please Enter ${ZTMP} (case sensitive): "
+      read JS_CONTAINER_NAME
+      if [ "${JS_CONTAINER_NAME}" == "" ]
+      then
+         echo "No ${ZTMP} Provided, Exiting ..."
+         exit 1;
+      fi
+   else
+      echo "No ${ZTMP} Provided, using Default ..."
+      JS_CONTAINER_NAME=${DEF_JS_CONTAINER_NAME}
+   fi
+fi
+echo "template container name: ${JS_CONTAINER_NAME}"
+
+JS_CONTAINER_REF=`echo "${STATUS}" | jq --raw-output '.result[] | select(.template=="'"${JS_TPL_REF}"'" and .name=="'"${JS_CONTAINER_NAME}"'") | .reference '`
+echo "template container reference: ${JS_CONTAINER_REF}"
+
+if [[ "${JS_CONTAINER_REF}" == "" ]]
+then
+   echo "${ZTMP} Reference ${JS_CONTAINER_REF} for ${JS_CONTAINER_NAME} not found, Exiting ..."
+   exit 1
+fi
+
+JS_DC_ACTIVE_BRANCH=`echo "${STATUS}" | jq --raw-output '.result[] | select(.template=="'"${JS_TPL_REF}"'" and .name=="'"${JS_CONTAINER_NAME}"'") | .activeBranch '`
+echo "Container Active Branch Reference: ${JS_DC_ACTIVE_BRANCH}"
+
+#########################################################
+## Get Active Branch Reference ...
+
+#echo "Getting Jetstream Branch Reference Value ..."
+STATUS=`curl -s -X GET -k ${BaseURL}/jetstream/branch/${JS_DC_ACTIVE_BRANCH} -b "${COOKIE}" -H "${CONTENT_TYPE}"`
+RESULTS=$( jqParse "${STATUS}" "status" )
+#echo "${STATUS}" | jq "."
+
+ACTIVE_BRANCH_NAME=`echo "${STATUS}" | jq --raw-output '.result.name'`
+echo "Active Branch Name: ${ACTIVE_BRANCH_NAME}"
+
+#
+# Muse use Active Branch ...
+#
+JS_BRANCH_REF="${JS_DC_ACTIVE_BRANCH}"
+
+if [[ "${JS_BRANCH_REF}" == "" ]]
+then
+   echo "Branch Reference ${JS_BRANCH_REF} for Active Branch not found, Exiting ..."
+   exit 1
+fi
+
+#echo "${STATUS}" | jq --raw-output '.result[] | select (.reference=="'"${JS_BRANCH_REF}"'")'
+
+#########################################################
+#
+# Get Remaining Command Line Parameters ...
+#
+
+JS_BOOK_NAME="${3}"
+if [[ "${JS_BOOK_NAME}" == "" ]] 
+then
+   if [[ "${DEF_JS_BOOK_NAME}" == "" ]]
+   then 
+      echo "---------------------------------"
+      echo "Please Enter Bookmark Name: "
+      read JS_BOOK_NAME
+      if [[ "${JS_BOOK_NAME}" == "" ]]
+      then
+         echo "No Bookmark Name Provided, Exiting ..."
+         exit 1;
+      fi
+   else 
+      echo "No Bookmark Name Provided, using Default ..."
+      JS_BOOK_NAME=${DEF_JS_BOOK_NAME}
+   fi
+fi
+
+SHARED="${4}"
+if [[ "${SHARED}" == "" ]] 
+then
+   if [[ "${DEF_SHARED}" == "" ]]
+   then 
+      echo "---------------------------------"
+      echo "Options: true | false "
+      echo "Please Enter Bookmark Sharing Option : "
+      read SHARED
+      if [[ "${SHARED}" == "" ]]
+      then
+         echo "No Bookmark Name Provided, Exiting ..."
+         exit 1;
+      fi
+   else    
+      echo "No Bookmark Shared Option Provided, using Default ..."
+      SHARED=${DEF_SHARED}
+   fi 
+fi
+
+TAGS="${5}"
+if [[ "${TAGS}" == "" ]]      
+then
+   if [[ "${DEF_TAGS}" == "" ]]
+   then  
+      echo "---------------------------------"
+      echo "Each value must be quoted and multiple tags delimited by comma"
+      echo "   Single Tag Example: \"REL1.3\""
+      echo "Multiple Tags Example: \"API Created\",\"REL123\""
+      echo "Please Enter Bookmark Tags (Enter for no tags): "
+      read TAGS
+      #if [[ "${TAGS}" == "" ]]
+      #then
+      #   echo "No Bookmark Tags Provided, Exiting ..."
+      #   exit 1;
+      #fi
+   else    
+      echo "No Bookmark Tags Provided, using Default ..."
+      TAGS=${DEF_TAGS}
+   fi 
+fi
+
+if [[ "${TAGS}" == "" ]]
+then
+  TAGS="\"\""
+fi
+
+#
+# TODO: Validate Command Line Parameter Values ...
+# 
+
+#########################################################
+# 
+# Create Bookmark ...
+#  Change parameters as required and desired :) 
+#
+json="
+{
+    \"type\": \"JSBookmarkCreateParameters\",
+    \"bookmark\": {
+        \"type\": \"JSBookmark\",
+        \"name\": \"${JS_BOOK_NAME}\",
+        \"branch\": \"${JS_BRANCH_REF}\",
+        \"shared\": ${SHARED},
+        \"tags\": [ ${TAGS} ]
+    },
+    \"timelinePointParameters\": {
+        \"type\": \"JSTimelinePointLatestTimeInput\",
+        \"sourceDataLayout\": \"${JS_CONTAINER_REF}\"
+    }
+}"
+
+echo "JSON: ${json}"
+
+STATUS=`curl -s -X POST -k --data @- $BaseURL/jetstream/bookmark -b "${COOKIE}" -H "${CONTENT_TYPE}" <<EOF
+${json}
+EOF
+`
+
+#
+# Note: the timelinePointParameters type "JSTimelinePointLatestTimeInput" is the last point / latest time in the branch!
+#
+
+#echo "JetStream Bookmark Creation Results: ${STATUS}"
+RESULTS=$( jqParse "${STATUS}" "status" )
+
+sleep 2
+
+#########################################################
+#
+# Get Job Number ...
+#
+JOB=$( jqParse "${STATUS}" "job" )
+echo "Job: ${JOB}"
+
+#########################################################
+#
+# Job Information ...
+#
+JOB_STATUS=`curl -s -X GET -k ${BaseURL}/job/${JOB} -b "${COOKIE}" -H "${CONTENT_TYPE}"`
+RESULTS=$( jqParse "${JOB_STATUS}" "status" )
+
+#########################################################
+#
+# Get Job State from Results, loop until not RUNNING  ...
+#
+JOBSTATE=$( jqParse "${JOB_STATUS}" "result.jobState" )
+PERCENTCOMPLETE=$( jqParse "${JOB_STATUS}" "result.percentComplete" )
+echo "Current status as of" $(date) ": ${JOBSTATE} ${PERCENTCOMPLETE}% Completed"
+while [ "${JOBSTATE}" == "RUNNING" ]
+do
+   sleep ${DELAYTIMESEC}
+   JOB_STATUS=`curl -s -X GET -k ${BaseURL}/job/${JOB} -b "${COOKIE}" -H "${CONTENT_TYPE}"`
+   JOBSTATE=$( jqParse "${JOB_STATUS}" "result.jobState" )
+   PERCENTCOMPLETE=$( jqParse "${JOB_STATUS}" "result.percentComplete" )
+   echo "Current status as of" $(date) ": ${JOBSTATE} ${PERCENTCOMPLETE}% Completed"
+done
+
+#########################################################
+##  Producing final status
+
+if [ "${JOBSTATE}" != "COMPLETED" ]
+then
+   echo "Error: Delphix Job Did not Complete, please check GUI ${JOB_STATUS}"
+#   exit 1
+else
+   echo "Job: ${JOB} ${JOBSTATE} ${PERCENTCOMPLETE}% Completed ..."
+fi
+
+############## E O F ####################################
+echo " "
+echo "Done ..."
+echo " "
+exit 0
+
