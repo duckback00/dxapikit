9eb6f5763a70d810a738017650b1b29173838f25
Alan Bitterman <duckback00@gmail.com>
2018-12-08 10:17:40 -0500

    Add files via upload

---
+++ Delphix_Reporting/dr_dump_months.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2018 by Delphix. All rights reserved.
+#
+# Program Name : dr_dump_all.sh
+# Description  : Delphix Reporting API Examples
+# Author       : Alan Bitterman
+# Created      : 2018-07-11
+# Version      : v1.0
+# Platforms    : ONLY works with Delphix Version 5.2.x or later
+#
+# Requirements :
+#  1.) curl and jq command line libraries
+#  2.) Change values below as required
+#
+# Usage:
+# ./dr_dump_all.sh 
+#
+#########################################################
+#                   DELPHIX CORP                        #
+# Please make changes to the parameters below as req'd! #
+#########################################################
+
+#########################################################
+## Parameter Initialization ...
+
+DRIP="172.16.160.134"		# Delphix Reporting Server IP Address/FullyQualified Hostname
+DRUSR="delphix_admin"		# Delphix Reporting User Account
+DRPWD="delphix"			# Delphix Reporting User Password
+
+#########################################################
+#         NO CHANGES REQUIRED BELOW THIS POINT          #
+#########################################################
+
+# Output Variables ...
+DT=`date '+%Y%m%d%H%M%S'`
+OUTFILE="DumpAll_${DRIP}_${DT}.json"
+
+echo "{ 
+  \"timeStamp\": \"${DT}\"
+, \"report_server\": \"${DRIP}\" " > ${OUTFILE}
+
+#########################################################
+## jq ...
+
+JQ=`which jq`
+if [[ "${JQ}" == "" ]] 
+then
+   echo "Please enter toolkit path: "
+   read TKP
+   TKP=`echo $TKP | grep -v ^$`
+   echo "Toolkit Path: ${TKP}"
+   # ./toolkit/Delphix_COMMON_420e34b0_0ce8_4555_3f82_6a8d25e540bb_oracle_host/scripts/jq/linux_x86/bin64/jq
+   list=$(find ${TKP} -name "*jq" | grep "linux_x86/bin64")
+   #echo "$list"
+   for i in "$list"; do 
+      if [[ "${i}" != "" ]] 
+      then
+         JQ=`echo "${i}" | grep -v ^$` 
+      fi
+   done
+fi
+if [[ "${JQ}" == "" ]]
+then
+   echo "ERROR: Unable to find jq, the JSON Command Line Utility ..."
+   echo "Either install the utility or run on a Delphix Source or Target server"
+   echo "Exiting ..."
+   exit 1
+fi
+echo "jq: ${JQ}"
+
+#########################################################
+## Authentication ...
+
+echo "Authenticating URL: http://${DRIP}/api/login ..."
+STATUS=`curl -s --data "password=${DRPWD}&user=${DRUSR}" http://${DRIP}/api/login`
+#echo "${STATUS}" | ${JQ} "."
+RESULTS=`echo "${STATUS}" | ${JQ} -r ".success"`
+if [[ "${RESULTS}" != "true" ]] 
+then 
+   echo "${STATUS}" | ${JQ} "."
+   echo "Error in authentication, exiting ..."
+   exit 1
+fi
+
+#
+# Parse results to get authentication key and userid ...
+#
+KEY=`echo "${STATUS}" | ${JQ} -r ".loginToken"`
+USERID=`echo "${STATUS}" | ${JQ} -r ".userId"`
+
+echo "Authentication Key: ${KEY}"
+echo "Authentication UserId: ${USERID}"
+
+#########################################################
+## List Reports ...
+
+echo "Reports ..."
+STATUS=`curl -s -H "X-Login-Token: ${KEY}" -H "X-User-Id: ${USERID}" "http://${DRIP}/api/list_reports"`
+#echo "${STATUS}" | ${JQ} "."
+
+#RPTLIST=`echo "${STATUS}" | ${JQ} -r '.[] | ._id'`
+#RPTLIST=`echo "${STATUS}" | ${JQ} -r '.[] | select (._id | contains("month")) | ._id'`
+RPTLIST=`echo "${STATUS}" | ${JQ} -r '.[] | select(._id | startswith("result_month_minus")) | ._id'`
+
+# result_js_bookmark_usage
+# result_table_capacity_breakdown
+# result_active_faults
+# result_vdb_invt
+# result_audit
+# result_recent_alerts
+# result_snapsync_summary
+# result_replication_status
+# result_engine_summary
+# result_storage_summary
+# result_vdb_usge
+# result_recent_jobs
+# result_dsource_usage
+# result_vdb_refresh
+# 
+#   {
+#     "_id": "result_storage_summary",
+#     "name": "Storage Summary",
+#     "script": "storage-summary.js"
+#   },
+
+echo "${RPTLIST}"
+
+#########################################################
+## Loop through each Report ...
+
+
+let k=0
+while read rid
+do
+   STATUS=""
+   let k=k+1
+   echo "$k ... $rid "
+   STATUS=`curl -s -H "X-Login-Token: ${KEY}" -H "X-User-Id: ${USERID}" "http://${DRIP}/api/get_report?report=${rid}"`
+   echo ", \"${rid}\": 
+ ${STATUS} " >> ${OUTFILE}
+done <<< "${RPTLIST}"
+
+echo "}" >> ${OUTFILE}
+echo "JSON Dump File: ${OUTFILE}"
+
+#########################################################
+## Logout ...
+
+#STATUS=`curl -s -H "X-Login-Token: ${KEY}" -H "X-User-Id: ${USERID}" "http://${DRIP}/api/logout"`
+#echo "${STATUS}" | ${JQ} "."
+
+#########################################################
+## The End is Here ...
+echo "Done ..."
+exit 0
+
---
+++ Delphix_Reporting/month_minus_1_jobs.js
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2014, 2015 by Delphix. All rights reserved.
+ */
+
+'use strict';
+
+load('lib/report-utils.js');
+
+var RESULT_COL = 'result_month_minus_1_jobs';
+
+var dependencies = [ 'Job' ];
+
+var limit = dx.defineTunableInt({
+    _id : 'prevMonth.limit',
+    defaultValue : 50000,
+    description : 'Maximum number of jobs to display',
+    report : RESULT_COL
+});
+
+dx.updateMetadata(RESULT_COL, {
+    name : 'Previous Month Jobs',
+    type: dx.report.type.TABLE,
+    category : dx.report.category.ACTIVITY,
+    script : 'month_minus_1_jobs.js',
+    tableHeaders : [ {
+        mData : '_delphixEngineId',
+        sTitle : 'Delphix Engine',
+        renderFn : 'link'
+    }, {
+        mData : 'title',
+        sTitle : 'Title'
+    }, {
+        mData : 'actionType',
+        sTitle : 'Action'
+    }, {
+        mData : 'jobState',
+        sTitle : 'Status'
+    }, {
+        mData : 'startTime',
+        sTitle : 'Start time',
+        sort : 'desc'
+    }, {
+        mData : 'updateTime',
+        sTitle : 'Update time'
+    }, {
+        mData : 'duration',
+        sTitle : 'Duration',
+        renderFn : 'duration'
+    }, {
+        mData : 'targetName',
+        sTitle : 'Target'
+    }, {
+        mData : 'user',
+        sTitle : 'User'
+    } ]
+});
+
+dx.checkDependencies(dependencies);
+
+db.Job.ensureIndex({
+    startTime : 1
+});
+
+var mm = new Date().getMonth()+1;        // base 0-11 ...
+var yyyy = new Date().getFullYear();
+//print (yyyy, mm);
+
+var mm = mm - 1;       // 0=current month, 1 through 12 previous months ...
+if (mm == 0) { var mm = 12; var yyyy = yyyy - 1 } 
+//print (yyyy, mm);
+
+var startDate = new Date(yyyy, mm-1, 1);   
+//print (startDate);
+
+var endDate = new Date(startDate.getFullYear(), startDate.getMonth()+1, 0);
+//print (endDate);
+
+var cond = {actionType : {$regex: '^DB_*'},"startTime": {'$gte': startDate, '$lte': endDate}};
+//printjson (cond);
+
+//db.Job.find(cond);
+//db.Job.find(cond).pretty();
+
+var result = db.Job.find( cond, {
+        _delphixEngineId : 1,
+        title : 1,
+        actionType : 1,
+        jobState : 1,
+        startTime : 1,
+        updateTime : 1,
+        targetName : 1,
+        user : 1
+});
+
+var jobs = result.toArray();
+
+var userNameMap = dx.getNameMap('User');
+
+jobs.forEach(function(doc) {
+    doc.duration = doc.updateTime - doc.startTime;
+    var userId = dx.getId(doc.user, doc._delphixEngineId);
+    doc.user = userNameMap[userId];
+});
+
+
+dx.saveResults(RESULT_COL, jobs);
